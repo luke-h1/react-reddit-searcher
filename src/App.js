e22694917b1422319cba844f614383caef95a771
luke-h1 <luke.howsam@yahoo.com>
2020-10-03 16:26:04 +0100

    add basic styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <header className="header">
+        <h1>Reddit Nature Search</h1>
+      </header>
+      <form class="form">
+        <input type="text" class="input-text" />
+        <input type="submit" class="input-submit" />
+      </form>
+      <div className="container">
+        <div className="grid-container">
+          <div className="card">
+            <img src="/test.jpeg" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
